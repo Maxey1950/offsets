eb9af50c12d9d1bfbaa3a76dca679c386baa3c13
Maxey <92372504+Maxey1950@users.noreply.github.com>
2025-12-27 12:08:50 -0500

    Update injectoroffsets.hpp

--- injectoroffsets.hpp
+++ injectoroffsets.hpp
@@ -1,6 +1,6 @@
 // rbx ver: version-5b077c09380f4fe6
 namespace offsets {
-  inline uintptr_t TaskSchedulerPointer = 0x7E1CB88;
-	inline uintptr_t TaskSchedulerToJobs = 0x1D0;
-	inline uintptr_t NtUnmapViewOfSectionSyscall = 0x72B527;
+  	inline uintptr_t TaskSchedulerPointer = 0x7E1CB88; // sometimes rawscheduler sometimes external scheduler.
+	inline uintptr_t TaskSchedulerToJobs = 0x1D0; //  basically just JobStart
+	inline uintptr_t NtUnmapViewOfSectionSyscall = 0x72B527; // this the injectors offset
 }
